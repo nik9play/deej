a4c97825e5331ebdc0cad26fadc6fa869b8a82ad
Nikita <n-bogun@mail.ru>
2025-01-31 18:48:35 +0300

    shut the linter

--- pkg/deej/util/util.go
+++ pkg/deej/util/util.go
@@ -52,8 +52,7 @@ func Linux() bool {
 // SetupCloseHandler creates a 'listener' on a new goroutine which will notify the
 // program if it receives an interrupt from the OS
 func SetupCloseHandler() chan os.Signal {
-	c := make(chan os.Signal)
-	//nolint:all
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
 	return c
